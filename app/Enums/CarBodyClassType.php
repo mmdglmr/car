0835126a780a932c76eabcc08d86642fd0612507
mmdglmr <mmdglmr@gmail.com>
2020-01-31 01:35:44 +0330

    modified car migration added car styles and compony builder (enum values)

---
+++ app/Enums/CarBodyClassType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class CarBodyClassType extends Enum
+{
+    const Hatchback = 0;
+    const Sedan = 1;
+    const SUV = 2;
+    const Coupe = 3;
+    const Convertible = 4;
+    const StationWagon = 5;
+    const Crossover = 7;
+    const MPV = 8;
+    const miniVan = 9;
+    const Pickup = 10;
+}
---
+++ app/Enums/CarBuilderCompanyType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class CarBuilderCompanyType extends Enum
+{
+    const IranKhodro = 0;
+    const Saipa = 1;
+}
