6e2b635965ce98600b4001674fa6b9122e680443
mmdglmr <mmdglmr@gmail.com>
2020-01-31 22:27:13 +0330

    added git theme to app

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,4 +1,9 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('car') }}'><i class='nav-icon fa fa-car'></i> انواع ماشین</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('carfeature') }}'><i class='nav-icon fa fa-sitemap'></i> ویژگی های ماشین</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard nav-icon"></i> داشبورد</a></li>
+<li class="nav-item nav-dropdown">
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="fa fa-car nav-icon"></i> ماشین </a>
+    <ul class="nav-dropdown-items">
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('car') }}'><i class='nav-icon fa fa-car'></i> انواع ماشین</a></li>
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('carfeature') }}'><i class='nav-icon fa fa-sitemap'></i> ویژگی های ماشین</a></li>
+    </ul>
+</li>
