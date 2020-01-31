251609c56961bdfae5ca3d4a3a90e7b667cef271
mmdglmr <mmdglmr@gmail.com>
2020-01-31 18:46:57 +0330

    Added fields to car and feature car

--- app/Http/Controllers/Admin/CarCrudController.php
+++ app/Http/Controllers/Admin/CarCrudController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\CarRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+use App\Http\Requests\RuleRequest as StoreRequest;
+use App\Http\Requests\RuleRequest as UpdateRequest;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+use App\Enums\CarBuilderCompanyType;
+use App\Enums\CarBodyClassType;
 
 /**
  * Class CarCrudController
@@ -13,11 +21,10 @@ use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
  */
 class CarCrudController extends CrudController
 {
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+    use ListOperation;
+    use CreateOperation { store as traitStore; }
+    use UpdateOperation { update as traitUpdate; }
+    use DeleteOperation;
 
     public function setup()
     {
@@ -34,14 +41,100 @@ class CarCrudController extends CrudController
 
     protected function setupCreateOperation()
     {
+        $this->crud->addFields($this->getFields());
         $this->crud->setValidation(CarRequest::class);
 
         // TODO: remove setFromDb() and manually define Fields
-        $this->crud->setFromDb();
+        // $this->crud->setFromDb();
     }
 
     protected function setupUpdateOperation()
     {
         $this->setupCreateOperation();
+    }
+
+    public function store()
+    {
+        $this->crud->request = $this->crud->validateRequest();
+        $this->crud->addField('car_features_id');
+        $this->crud->request->request->set('car_features_id', 1);
+        $this->crud->unsetValidation();
+        $redirect_location = $this->traitStore();
+        return $redirect_location;
+    }
+
+    protected function getFields()
+    {
+        return [
+            'builder_company' => [
+                'name' => 'builder_company',
+                'label' => "کمپانی سازنده",
+                'type' => 'select2_from_array',
+                'options' => CarBuilderCompanyType::getkeys(),
+                'allows_null' => false,
+                'attributes' => [
+                    'required' => 'required',
+                ],
+                'wrapperAttributes' => [
+                    'class' => 'form-group col-md-6',
+                ],
+            ],
+            'body_class' => [
+                'name' => 'body_class',
+                'label' => "کلاس بدنه",
+                'type' => 'select2_from_array',
+                'options' => CarBodyClassType::getkeys(),
+                'allows_null' => false,
+                'attributes' => [
+                    'required' => 'required',
+                ],
+                'wrapperAttributes' => [
+                    'class' => 'form-group col-md-6',
+                ],
+            ],
+            'brand' => [
+                'label' => 'کمپانی اصلی خودرو',
+                'name' => 'brand',
+                'type' => 'text',
+                'attributes' => [
+                    'placeholder' => 'پژو-رنو ...',
+                    'required' => 'required'
+                ],
+                'wrapperAttributes' => [
+                    'class' => 'form-group col-md-6',
+                ],
+            ],
+            'model' => [
+                'label' => 'مدل خودرو',
+                'name' => 'model',
+                'type' => 'text',
+                'attributes' => [
+                    'placeholder' =>'پارس - ۲۰۶ و ...',
+                    'required' => 'required'
+                ],
+                'wrapperAttributes' => [
+                    'class' => 'form-group col-md-6',
+                ],
+            ],
+            'series' => [
+                'label' => 'تیپ خودرو',
+                'name' => 'series',
+                'type' => 'text',
+                'attributes' => [
+                    'placeholder' =>'برای ۲۰۶: تیپ۲- صندوقدار و ...',
+                ],
+                'wrapperAttributes' => [
+                    'class' => 'form-group col-md-6',
+                ],
+            ],
+            'description' => [
+                'name' => 'description',
+                'type' => 'textarea',
+                'label' => 'توضیحات',
+                'wrapperAttributes' => [
+                    'class' => 'form-group col-md-6',
+                ],
+            ],
+        ];
     }
 }
--- app/Http/Controllers/Admin/CarFeatureCrudController.php
+++ app/Http/Controllers/Admin/CarFeatureCrudController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\CarFeatureRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+use App\Http\Requests\RuleRequest as StoreRequest;
+use App\Http\Requests\RuleRequest as UpdateRequest;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 
 /**
@@ -13,11 +19,10 @@ use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
  */
 class CarFeatureCrudController extends CrudController
 {
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+    use ListOperation;
+    use CreateOperation { store as traitStore; }
+    use UpdateOperation { update as traitUpdate; }
+    use DeleteOperation;
 
     public function setup()
     {
@@ -34,14 +39,139 @@ class CarFeatureCrudController extends CrudController
 
     protected function setupCreateOperation()
     {
+        $this->crud->addFields($this->getFields());
         $this->crud->setValidation(CarFeatureRequest::class);
-
-        // TODO: remove setFromDb() and manually define Fields
-        $this->crud->setFromDb();
     }
 
     protected function setupUpdateOperation()
     {
         $this->setupCreateOperation();
+    }
+
+
+        protected function getFields()
+        {
+            return [
+                'engine_volume' => [
+                    'label' => 'حجم موتور',
+                    'name' => 'engine_volume',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'engine_power' => [
+                    'label' => 'قدرت موتور',
+                    'name' => 'engine_power',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'number_of_cylinders' => [
+                    'label' => 'تعداد سیلندر',
+                    'name' => 'number_of_cylinders',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'number_of_valves' => [
+                    'label' => 'تعداد سوپاپ',
+                    'name' => 'number_of_valves',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'torque' => [
+                    'label' => 'گشتاور',
+                    'name' => 'torque',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'acceleration' => [
+                    'label' => 'شتاب',
+                    'name' => 'acceleration',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'max_speed' => [
+                    'label' => 'حداکثر سرعت',
+                    'name' => 'max_speed',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'gearbox' => [
+                    'label' => 'گیربکس',
+                    'name' => 'gearbox',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'differential' => [
+                    'label' => 'دیفرانسیل',
+                    'name' => 'differential',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'car_dimensions' => [
+                    'label' => 'ابعاد خودرو',
+                    'name' => 'car_dimensions',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'axis_spacing' => [
+                    'label' => 'فاصله محور',
+                    'name' => 'axis_spacing',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'fuel_consumption' => [
+                    'label' => 'مصرف سوخت',
+                    'name' => 'fuel_consumption',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'fuel_tank_capacity' => [
+                    'label' => 'حجم باک',
+                    'name' => 'fuel_tank_capacity',
+                    'type' => 'text',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'safety' => [
+                    'name' => 'safety',
+                    'type' => 'textarea',
+                    'label' => 'امنیبت',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+                'options' => [
+                    'name' => 'options',
+                    'type' => 'textarea',
+                    'label' => 'امکانات',
+                    'wrapperAttributes' => [
+                        'class' => 'form-group col-md-6',
+                    ],
+                ],
+            ];
     }
 }
